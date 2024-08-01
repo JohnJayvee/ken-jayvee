3b08e1b9ae8b5ecfc33594f6acc1c840fd393281
Ken-2100 <kenmarkamandoron2100@gmail.com>
2024-08-01 17:49:13 +0800

    fix FoodItem.jsx fetching

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,20 +1,27 @@
 import useAuth from "./auth";
 
-export default function CartItem({itemName, quantity, price, onIncrease, onDecrease}){
-    const isLoggedIn = useAuth();
-if (!isLoggedIn) {
+export default function CartItem({
+  imageItem,
+  itemName,
+  quantity,
+  price,
+  onIncrease,
+  onDecrease,
+}) {
+  const isLoggedIn = useAuth();
+  if (!isLoggedIn) {
     return null;
-  };
-    return <li className="cart-item">
-        <p>
-        {itemName} - {quantity} x {`₱${price}`}
-        </p>
-        <div className="cart-item-actions">
-            <button onClick={onDecrease}>-</button>
-            <span> {quantity}</span>
-            <button onClick={onIncrease}>+</button>
-        </div>
-
-    
+  }
+  return (
+    <li className="cart-item">
+      <p>
+        {imageItem} {itemName} - {quantity} x {`₱${price}`}
+      </p>
+      <div className="cart-item-actions">
+        <button onClick={onDecrease}>-</button>
+        <span> {quantity}</span>
+        <button onClick={onIncrease}>+</button>
+      </div>
     </li>
+  );
 }
--- src/components/Food-Products/FoodItem.jsx
+++ src/components/Food-Products/FoodItem.jsx
@@ -1,29 +1,31 @@
 import { useContext } from "react";
-import CartContext from '../../store/CartContext';
+import CartContext from "../../store/CartContext";
 import Button from "../UI/Button";
 
-export default function FoodItem({foods}){
-    const cartCtx = useContext(CartContext);
-    function handleAddFoodToCart(){
-        cartCtx.addItem(foods)
+export default function FoodItem({ foods }) {
+  const cartCtx = useContext(CartContext);
+  function handleAddFoodToCart() {
+    cartCtx.addItem(foods);
+  }
+  return (
+    <li className="food-item">
+      <div className="item-card">
+        <img src={foods.images[0]} alt={foods.category.name} />
+        <div>
+          <h3>{foods.title}</h3>
 
+          <p className="food-item-description">
+            {foods.description.substring(0, 60)}
+          </p>
+        </div>
 
-    }
-    return(
-        <li className="food-item">
-            <div className="item-card">
-                <img src={foods.category.image} alt={foods.category.name}/>
-                <div >
-                    <h3>{foods.category.name}</h3>
-                   
-                    <p className="food-item-description">{foods.description.substring(0,60)}</p>
-                </div>
-    
-                <div className="food-item-actions d-flex justify-content-around">
-                <dt className="food-item-price">₱{foods.price}</dt>
-                    <Button className="btn-outline-light" onClick={handleAddFoodToCart}>Add to  Cart</Button>
-                </div>
-            </div>
-        </li>
-    )
+        <div className="food-item-actions d-flex justify-content-around">
+          <dt className="food-item-price">₱{foods.price}</dt>
+          <Button className="btn-outline-light" onClick={handleAddFoodToCart}>
+            Add to Cart
+          </Button>
+        </div>
+      </div>
+    </li>
+  );
 }
