ff02cfd825d41cfaa92441718cd43bdb161159e8
Ken-2100 <kenmarkamandoron2100@gmail.com>
2024-08-06 00:21:43 +0800

    show image with api fooditem and cartItem second commit

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -46,42 +46,7 @@ export default function Shop() {
           ))}
         </ul>
       </div>
-      <nav aria-label="pagination">
-        <ul class="pagination">
-          <li>
-            <a href="">
-              <span aria-hidden="true">«</span>
-              <span class="visuallyhidden">previous set of pages</span>
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span class="visuallyhidden">page </span>1
-            </a>
-          </li>
-          <li>
-            <a href="" aria-current="page">
-              <span class="visuallyhidden">page </span>2
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span class="visuallyhidden">page </span>3
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span class="visuallyhidden">page </span>4
-            </a>
-          </li>
-          <li>
-            <a href="">
-              <span class="visuallyhidden">next set of pages</span>
-              <span aria-hidden="true">»</span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+
       <Footer />
     </>
   );
