ae1624e166ea7c22570fd80470bab8a13f489ed7
JohnJayvee <siuagan.jayvee@gmail.com>
2024-08-16 20:21:37 +0800

    Updates

---
+++ assets/AboutUs-IdfQpKeg.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-3KMGfd71.js";import{G as o}from"./Goals-C8nD8pUc.js";import{H as t,F as s}from"./Footer-Ca7LG2Dt.js";import"./Button-BR-YezWj.js";import"./BaseUrl-DVqEL8bQ.js";import"./axios-B4uVmeYG.js";import"./auth-DfXIjouS.js";function n(){return r.jsxs(r.Fragment,{children:[r.jsx(t,{}),r.jsx(o,{}),r.jsx(s,{})]})}export{n as default};
