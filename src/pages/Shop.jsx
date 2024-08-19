6f122c2a888a903d5a5e561c6e81c9f1e98433eb
Ken-2100 <kenmarkamandoron2100@gmail.com>
2024-08-19 18:28:50 +0800

    resize shop foodItem

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -27,23 +27,25 @@ export default function Shop() {
   return (
     <>
       <Header />
-      <p className="h1 text-center">Shop</p>
-      <div className="container-fluid">
-        <p className="h3">{`Total Products: ${loadedItem.totalProducts}`}</p>
-        <ul className="row h-50" id="items">
-          {/* 1. Modify the useHttp hook to return the full response object instead of extracting and returning just the products array.
+      <div className="">
+        <p className="h1 text-center">Shop</p>
+        <div className="container-fluid">
+          <p className="h3">{`Total Products: ${loadedItem.totalProducts}`}</p>
+          <ul className="row h-50" id="items">
+            {/* 1. Modify the useHttp hook to return the full response object instead of extracting and returning just the products array.
         2. In the Shop component, use the dot notation to access the products array from the loadedItem object. */}
-          {loadedItem &&
-            loadedItem.products &&
-            loadedItem.products.map((items) => (
-              <div
-                className="col col-lg-2 col-md-5 my-2 justify-content-sm-center"
-                key={items.id}
-              >
-                <FoodItem key={items.id} foods={items} />
-              </div>
-            ))}
-        </ul>
+            {loadedItem &&
+              loadedItem.products &&
+              loadedItem.products.map((items) => (
+                <div
+                  className="col col-lg-2 col-md-5 my-2 justify-content-sm-center"
+                  key={items.id}
+                >
+                  <FoodItem key={items.id} foods={items} />
+                </div>
+              ))}
+          </ul>
+        </div>
       </div>
 
       <Footer />
