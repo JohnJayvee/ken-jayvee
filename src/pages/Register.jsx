cc12b40fddd291c7446cbbb2d67372f3a4b9caf3
JohnJayvee <siuagan.jayvee@gmail.com>
2024-08-31 11:23:51 +0800

    add validation error

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,12 +1,13 @@
 import Button from "../components/UI/Button";
 import Input from "../components/UI/InputBlock";
-import { Link, redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logoSVG from "./logo-transparent.png";
 import { useState } from "react";
 import axios from "axios";
 
 export default function Register() {
   const [generalError, setGeneralError] = useState("");
+  const [fieldErrors, setFieldErrors] = useState({});
   const [isSending, setIsSending] = useState(false);
   const [formData, setFormData] = useState({
     name: "",
@@ -36,6 +37,7 @@ export default function Register() {
   const handleSubmit = async (event) => {
     event.preventDefault();
     setGeneralError("");
+    setFieldErrors({});
     setIsSending(true);
 
     // Prepare form data with file upload
@@ -67,9 +69,13 @@ export default function Register() {
     } catch (error) {
       // Error handling
       if (error.response && error.response.status === 422) {
-        setGeneralError(error.response.data.errors);
+        setFieldErrors(error.response.data.errors);
+        // setGeneralError("Please fix the highlighted errors.");
+        setGeneralError("Please check all the fields");
+
       } else {
         console.error("Error submitting registration form:", error);
+        setGeneralError("An unexpected error occurred. Please try again.");
       }
     } finally {
       setIsSending(false);
@@ -96,6 +102,7 @@ export default function Register() {
                   className="text-dark"
                   onSubmit={handleSubmit}
                   encType="multipart/form-data"
+                  noValidate
                 >
                   <Input
                     type="text"
@@ -105,6 +112,9 @@ export default function Register() {
                     onChange={handleChange}
                     value={formData.name}
                   />
+                  {fieldErrors.name && (
+                    <div className="text-danger">{fieldErrors.name[0]}</div>
+                  )}
                   <Input
                     type="text"
                     name="email"
@@ -113,6 +123,9 @@ export default function Register() {
                     onChange={handleChange}
                     value={formData.email}
                   />
+                  {fieldErrors.email && (
+                    <div className="text-danger">{fieldErrors.email[0]}</div>
+                  )}
                   <Input
                     type="text"
                     name="username"
@@ -121,6 +134,9 @@ export default function Register() {
                     onChange={handleChange}
                     value={formData.username}
                   />
+                  {fieldErrors.username && (
+                    <div className="text-danger">{fieldErrors.username[0]}</div>
+                  )}
                   <Input
                     type="password"
                     name="password"
@@ -129,6 +145,9 @@ export default function Register() {
                     onChange={handleChange}
                     value={formData.password}
                   />
+                  {fieldErrors.password && (
+                    <div className="text-danger">{fieldErrors.password[0]}</div>
+                  )}
                   <Input
                     type="password"
                     name="password_confirmation"
@@ -137,6 +156,11 @@ export default function Register() {
                     onChange={handleChange}
                     value={formData.password_confirmation}
                   />
+                  {fieldErrors.password_confirmation && (
+                    <div className="text-danger">
+                      {fieldErrors.password_confirmation[0]}
+                    </div>
+                  )}
                   <Input
                     type="file"
                     name="image"
@@ -144,6 +168,9 @@ export default function Register() {
                     label="Profile Image"
                     onChange={handleChange}
                   />
+                  {fieldErrors.image && (
+                    <div className="text-danger">{fieldErrors.image[0]}</div>
+                  )}
                   <Button
                     className="btn-dark w-100"
                     type="submit"
