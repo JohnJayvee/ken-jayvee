60ec0e288ae9c5d95ac101943bc8b67f09cb3038
Ken-2100 <kenmarkamandoron2100@gmail.com>
2024-09-03 19:00:33 +0800

    Updates

--- assets/AboutUs-DNaZ0NFz.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BEVTC-w-.js";import{G as o}from"./Goals-DBwhvPfC.js";import{H as t,F as s}from"./Footer-BeCJlqby.js";import"./Button-DmfUAfPi.js";import"./BaseUrl-6_kCCrH9.js";import"./axios-B4uVmeYG.js";import"./auth-DIC6NoV1.js";function n(){return r.jsxs(r.Fragment,{children:[r.jsx(t,{}),r.jsx(o,{}),r.jsx(s,{})]})}export{n as default};
